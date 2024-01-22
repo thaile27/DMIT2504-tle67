c0cc8fd83eb40137da7c6c71158b09dfac376040
thaile27 <haile27@hotmail.ca>
2024-01-22 09:20:50 -0700

    completed assignment 1

--- assignments/assignment01/lib/main.dart
+++ assignments/assignment01/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, todo, sized_box_for_whitespace
+// ignore_for_file: use_key_in_widget_constructors, todo, sized_box_for_whitespace, file_name
 
 import 'package:flutter/material.dart';
 
@@ -57,56 +57,67 @@ class MyApp extends StatelessWidget {
             ),
             Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    decoration: const BoxDecoration(color: Colors.yellow),
-                    child: const Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Text('Container 3'),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      decoration: const BoxDecoration(color: Colors.yellow),
+                      child: const Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text('Container 3'),
+                      ),
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    decoration: const BoxDecoration(color: Colors.blue),
-                    child: const Align(
-                      alignment: Alignment.centerRight,
-                      child: Text('Container 4'),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      decoration: const BoxDecoration(color: Colors.blue),
+                      child: const Align(
+                        alignment: Alignment.centerRight,
+                        child: Text('Container 4'),
+                      ),
                     ),
                   ),
                 )
               ],
             ),
             Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                    width: 100,
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.black,
+                          border: Border.all(color: Colors.white, width: 3)),
+                      child: const Center(
+                        child: Text("Container 5",
+                            style: TextStyle(color: Colors.white)),
+                      )),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
                     height: 100,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.black,
-                        border: Border.all(color: Colors.white, width: 3)),
-                    child: const Center(
-                      child: Text("Container 5",
-                          style: TextStyle(color: Colors.white)),
-                    )),
-                Container(
-                  height: 100,
-                  width: 100,
-                  decoration: const BoxDecoration(
-                      shape: BoxShape.rectangle, color: Colors.red),
-                  child: const Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      "Con 6",
-                      style: TextStyle(fontSize: 30),
+                    width: 100,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.rectangle, color: Colors.red),
+                    child: const Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Con 6",
+                        style: TextStyle(fontSize: 30),
+                      ),
                     ),
                   ),
                 )
