826805688be0fe0c1aeac6678ce820ec742994bc
thaile27 <haile27@hotmail.ca>
2024-03-17 15:35:47 -0600

    added assignment 03 folder

---
+++ assignments/assignment03/docs/specs.md
@@ -0,0 +1,34 @@
+# Assignment 4: Stock Ticker
+
+## Overview
+
+The `start-kit` code given to you should compile and run as is, even though the functionality of the running app is not complete.
+
+You will see `TODO` items in the given code, that directs where to put your code to complete the assessment. 
+
+Commit your work and upload to a remote github repo and then submit the link to Moodle.
+
+## Required Tasks
+
+- Each `TODO:` comment will describe the expected functionality your code is to provide.
+
+- Place your code immediately below the `TODO:` comment.
+
+- `Do NOT change any of the code outside of a TODO: comment unless required`.
+
+### Part 1:
+
+- Modify methods of the `SQFliteDbService` class in the `services/db-service.dart` file:
+- Modify methods of the `StockService` class in the `services/stock-service.dart` file:
+
+### Part 2:
+
+- Modify the following method of the `HomeView` class in the `views/home.dart` file:
+  - `_inputStock()` to insert a stock into the database when the `Add Stock` button is pressed.
+
+## Sample Screen Shots
+ 
+![Screenshot1](./img/1.png)
+
+![Screenshot2](./img/2.png)
+
---
+++ assignments/assignment03/lib/services/db-service.dart
@@ -0,0 +1,81 @@
+// ignore_for_file: todo, avoid_print, library_prefixes, avoid_function_literals_in_foreach_calls, file_names, unused_import
+
+import 'package:path/path.dart' as pathPackage;
+import 'package:sqflite/sqflite.dart' as sqflitePackage;
+
+class SQFliteDbService {
+  late sqflitePackage.Database db;
+  late String path;
+
+  Future<void> getOrCreateDatabaseHandle() async {
+    try {
+      //TODO: Put your code here to complete this method.
+      var databasesPath = await sqflitePackage.getDatabasesPath();
+      path = pathPackage.join(databasesPath, 'stocks.db');
+    } catch (e) {
+      print('SQFliteDbService getOrCreateDatabaseHandle: $e');
+    }
+  }
+
+  Future<void> printAllStocksInDbToConsole() async {
+    try {
+      //TODO: Put your code here to complete this method.
+    } catch (e) {
+      print('SQFliteDbService printAllStocksInDbToConsole: $e');
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getAllStocksFromDb() async {
+    try {
+      //TODO: Put your code here to complete this method.
+      // Replace this return with valid data.
+      return <Map<String, dynamic>>[];
+    } catch (e) {
+      print('SQFliteDbService getAllStocksFromDb: $e');
+      return <Map<String, dynamic>>[];
+    }
+  }
+
+  Future<void> deleteDb() async {
+    try {
+      //TODO: Put your code here to implement this method.
+      print('Not Implemented Yet');
+    } catch (e) {
+      print('SQFliteDbService deleteDb: $e');
+    }
+  }
+
+  Future<void> insertStock(Map<String, dynamic> stock) async {
+    try {
+      //TODO:
+      //Put code here to insert a stock into the database.
+      //Insert the Stock into the correct table.
+      //Also specify the conflictAlgorithm.
+      //In this case, if the same stock is inserted
+      //multiple times, it replaces the previous data.
+
+    } catch (e) {
+      print('SQFliteDbService insertStock: $e');
+    }
+  }
+
+  Future<void> updateStock(Map<String, dynamic> stock) async {
+    try {
+      //TODO:
+      //Put code here to update stock info.
+
+    } catch (e) {
+      print('SQFliteDbService updateStock: $e');
+    }
+  }
+
+  Future<void> deleteStock(Map<String, dynamic> stock) async {
+    try {
+      //TODO:
+      //Put code here to delete a stock from the database.
+
+    } catch (e) {
+      print('SQFliteDbService deleteStock: $e');
+    }
+  }
+}
